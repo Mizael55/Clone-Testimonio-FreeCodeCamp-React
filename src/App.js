2cc296a9606f1d12bd86dc5945ef76a641ece7d9
Mizael55 <misaellsoler@gmail.com>
2023-05-31 17:02:43 -0400

    adding img dynamically

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp:</h1>
       <Testimonio 
-      nombre='Emma'
+      img='emma'
+      nombre='Emma Kisomoto'
       pais='Suecia'
       cargo='Ingeniera de Software'
       empresa='Spotify'
@@ -16,14 +17,16 @@ function App() {
 
       />
       <Testimonio 
-      nombre='Rosa Julia '
+      img='sarah'
+      nombre='Sarah Julia '
       pais='Brazil'
       cargo='Ingeniera de Software'
       empresa='Amazon'
       testimonio='Ut ipsum quis cupidatat commodo velit labore elit mollit culpa mollit laborum eiusmod. Amet magna sint aliquip cupidatat elit do velit minim sit magna aute sunt sit eiusmod. Pariatur deserunt quis minim nulla sunt. Sit id elit adipisicing deserunt incididunt eiusmod minim eiusmod in sunt commodo. Ex proident nulla velit deserunt sint sunt cupidatat qui elit. Est anim veniam incididunt consequat labore consectetur. Velit occaecat est dolore dolor mollit Lorem tempor cillum eiusmod tempor eu.'
       />
       <Testimonio 
-      nombre='Naz Mendez '
+      img='shawn'
+      nombre='shawn Mendez '
       pais='China'
       cargo='Ingeniera de Software'
       empresa='Google'
