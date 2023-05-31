b33b1877a3b8c35c7358e8313c5d9da8c405411b
Mizael55 <misaellsoler@gmail.com>
2023-05-31 16:51:38 -0400

    clone of freeCodeCamp testimonio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Testimonio from './components/testimonio.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp:</h1>
+      <Testimonio 
+      nombre='Emma'
+      pais='Suecia'
+      cargo='Ingeniera de Software'
+      empresa='Spotify'
+      testimonio='Ut ipsum quis cupidatat commodo velit labore elit mollit culpa mollit laborum eiusmod. Amet magna sint aliquip cupidatat elit do velit minim sit magna aute sunt sit eiusmod. Pariatur deserunt quis minim nulla sunt. Sit id elit adipisicing deserunt incididunt eiusmod minim eiusmod in sunt commodo. Ex proident nulla velit deserunt sint sunt cupidatat qui elit. Est anim veniam incididunt consequat labore consectetur. Velit occaecat est dolore dolor mollit Lorem tempor cillum eiusmod tempor eu.'
+
+      />
+      <Testimonio 
+      nombre='Rosa Julia '
+      pais='Brazil'
+      cargo='Ingeniera de Software'
+      empresa='Amazon'
+      testimonio='Ut ipsum quis cupidatat commodo velit labore elit mollit culpa mollit laborum eiusmod. Amet magna sint aliquip cupidatat elit do velit minim sit magna aute sunt sit eiusmod. Pariatur deserunt quis minim nulla sunt. Sit id elit adipisicing deserunt incididunt eiusmod minim eiusmod in sunt commodo. Ex proident nulla velit deserunt sint sunt cupidatat qui elit. Est anim veniam incididunt consequat labore consectetur. Velit occaecat est dolore dolor mollit Lorem tempor cillum eiusmod tempor eu.'
+      />
+      <Testimonio 
+      nombre='Naz Mendez '
+      pais='China'
+      cargo='Ingeniera de Software'
+      empresa='Google'
+      testimonio='Ut ipsum quis cupidatat commodo velit labore elit mollit culpa mollit laborum eiusmod. Amet magna sint aliquip cupidatat elit do velit minim sit magna aute sunt sit eiusmod. Pariatur deserunt quis minim nulla sunt. Sit id elit adipisicing deserunt incididunt eiusmod minim eiusmod in sunt commodo. Ex proident nulla velit deserunt sint sunt cupidatat qui elit. Est anim veniam incididunt consequat labore consectetur. Velit occaecat est dolore dolor mollit Lorem tempor cillum eiusmod tempor eu.'
+      />
+      </div>
     </div>
   );
 }
